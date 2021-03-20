e8a5e4193ec2088ec45c7995ba6881e040ab7e99
Sharan107 <75675225+Sharan107@users.noreply.github.com>
2021-03-20 12:31:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ const Constraint=Matter.Constraint;
 
 var score=0;
 var count;
+var ball;
 
 function setup() {
 createCanvas(1200,600);
@@ -15,40 +16,74 @@ createCanvas(1200,600);
 
   ground= new Ground(600,550,1200,10);
  
-  basketBase= new Basket(200,400,200,10);
+  basketBase= new Basket(900,450,20,200);
+  basketBase2= new Basket(1100,450,20,200);
   basketWall= new Basket(basketBase.x-100,basketBase.y,10,150);
   basketWall2=new Basket(basketBase.x+100,basketBase.y,10,150);
 
-  Engine.run(engine);
+     ball= Bodies.circle(200,300,30);
+   World.add(world,ball);
+    slingshot=new Rope(ball,{x:350,y:350})
 }
 
 function draw() {
-  background("pink")
-
+  background("white")
+  Engine.update(engine);
   ground.display();
   
-  count=count+1;
-
-if(frameCount%80==0){
-  frameCount=count;
-  block= new Block(random(0,1200),random(0,300),30,40);
-  block.display(); 
-  console.log(count);
-
-  if(block.y>basketBase.y){
-    score=score+1;
-  }
-}
+getTime();
 
 basketBase.display();
+basketBase2.display();
 basketWall.display();
 basketWall2.display();
+ellipse(ball.position.x,ball.position.y,30,30);
+slingshot.display();
+//Matter.Body.setPosition(basketBase.body,{x:mouseX,y:mouseY}) 
 
-Matter.Body.setPosition(basketBase.body,{x:mouseX,y:mouseY}) 
+if(ball.x>=basketWall.x && ball.y>=basketWall.y){
+  score=score+10;
+}
+if(ball.x>=basketWall2.x&&ball.y>=basketWall2.y){
+  score=score+10;
+}
+
 
  textSize(20);
  fill("white");
  stroke("darkBlue");
  strokeWeight(3);
  text("score: "+score,100,50);
+}
+function mouseDragged(){
+  Matter.Body.setPosition(ball, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+  slingshot.fly();
+}
+
+function keyPressed(){
+  if(keyCode===32){
+    Matter.body.setPosition(ball.body,{x:200,y:300});
+    slingshot.attach(ball.body,{x:350,y:350});
+  }
+}
+
+async function getBackgroundImg(){
+  var response=await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata")
+  var responseJSON=await response.json();
+  console.log(responseJSON)
+
+  var datetime = responseJSON.datetime
+  var hour = daytime.splice(11,13)
+
+  if(hour>=06 && hour<=19){
+    background("DarkBlue");
+  }
+  else{
+    background("white");
+  }
+
 }
